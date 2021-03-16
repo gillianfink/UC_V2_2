e1e6cb8b69ef079ce6bae870b085e6d734749b17
gillianfink <36948522+gillianfink@users.noreply.github.com>
2021-03-15 22:22:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,764 @@
+$(window).scroll(function () {
+    if ($(document).scrollTop() == 0) {
+        $('#header_nav').removeClass('small');
+        $('#header_nav').removeClass('fade');
+        // $('body').removeClass('green');
+        // $(‘body’).removeClass(‘changeColor’)
+
+    } else {
+        $('#header_nav').addClass('small');
+        $('#header_nav').addClass('fade');
+        // $('body').addClass('green');
+        // $(‘body’).addClass(‘changeColor’)
+    }
+});
+
+
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+
+$(document) .ready(function() {
+
+
+var a= true;
+    $("#madisontxt").hover(function(){
+        if (a==true) {
+          
+            $( "#madisonimg" ).css("display", "inherit");
+            
+            a=false;
+        }
+        else {
+
+            $( "#madisonimg" ).css("display", "none");
+
+            a=true;
+            
+        }   
+    });
+
+
+
+var b= true;
+    $("#allisontxt").hover(function(){
+        if (b==true) {
+          
+            $( "#allisonimg" ).css("display", "inherit");
+            
+            b=false;
+        }
+        else {
+
+            $( "#allisonimg" ).css("display", "none");
+
+            b=true;
+            
+        }   
+    });
+
+
+var c= true;
+    $("#zoetxt").hover(function(){
+        if (c==true) {
+          
+            $( "#zoeimg" ).css("display", "inherit");
+            
+            c=false;
+        }
+        else {
+
+            $( "#zoeimg" ).css("display", "none");
+
+            c=true;
+            
+        }   
+    });
+
+ var d= true;
+    $("#toritxt").hover(function(){
+        if (d==true) {
+          
+            $( "#toriimg" ).css("display", "inherit");
+            
+            d=false;
+        }
+        else {
+
+            $( "#toriimg" ).css("display", "none");
+
+            d=true;
+            
+        }   
+    });
+
+
+ var e= true;
+    $("#gracetxt").hover(function(){
+        if (e==true) {
+          
+            $( "#graceimg" ).css("display", "inherit");
+            
+            e=false;
+        }
+        else {
+
+            $( "#graceimg" ).css("display", "none");
+
+            e=true;
+            
+        }   
+    });
+
+var f= true;
+    $("#gilliantxt").hover(function(){
+        if (f==true) {
+          
+            $( "#gillianimg" ).css("display", "inherit");
+            
+            f=false;
+        }
+        else {
+
+            $( "#gillianimg" ).css("display", "none");
+
+            f=true;
+            
+        }   
+    });
+
+var g= true;
+    $("#hawatxt").hover(function(){
+        if (g==true) {
+          
+            $( "#hawaimg" ).css("display", "inherit");
+            
+            g=false;
+        }
+        else {
+
+            $( "#hawaimg" ).css("display", "none");
+
+            g=true;
+            
+        }   
+    });
+
+ var ggg= true;
+    $("#nikoletxt").hover(function(){
+        if (ggg==true) {
+          
+            $( "#nikoleimg" ).css("display", "inherit");
+            
+            ggg=false;
+        }
+        else {
+
+            $( "#nikoleimg" ).css("display", "none");
+
+            ggg=true;
+            
+        }   
+    });
+
+
+ var i= true;
+    $("#judytxt").hover(function(){
+        if (i==true) {
+          
+            $( "#judyimg1" ).css("display", "inherit");
+            $( "#judyimg2" ).css("display", "inherit");
+            
+            i=false;
+        }
+        else {
+
+            $( "#judyimg1" ).css("display", "none");
+            $( "#judyimg2" ).css("display", "none");
+
+            i=true;
+            
+        }   
+    });
+
+
+  var j= true;
+    $("#aboutfacetxt").hover(function(){
+        if (j==true) {
+          
+            $( "#aboutfaceimg1" ).css("display", "inherit");
+            $( "#aboutfaceimg2" ).css("display", "inherit");
+            
+            j=false;
+        }
+        else {
+
+            $( "#aboutfaceimg1" ).css("display", "none");
+            $( "#aboutfaceimg2" ).css("display", "none");
+
+            j=true;
+            
+        }   
+    });
+
+  var k= true;
+    $("#ladidatxt").hover(function(){
+        if (k==true) {
+          
+            $( "#ladidaimg1" ).css("display", "inherit");
+            $( "#ladidaimg2" ).css("display", "inherit");
+            $( "#ladidaimg3" ).css("display", "inherit");
+            
+            k=false;
+        }
+        else {
+
+            $( "#ladidaimg1" ).css("display", "none");
+            $( "#ladidaimg2" ).css("display", "none");
+            $( "#ladidaimg3" ).css("display", "none");
+
+            k=true;
+            
+        }   
+    });
+
+  var ab= true;
+    $("#muthatxt").hover(function(){
+        if (ab==true) {
+          
+            $( "#muthaimg1" ).css("display", "inherit");
+            $( "#muthaimg2" ).css("display", "inherit");
+            
+            ab=false;
+        }
+        else {
+
+            $( "#muthaimg1" ).css("display", "none");
+            $( "#muthaimg2" ).css("display", "none");
+
+            ab=true;
+            
+        }   
+    });
+
+   var bc= true;
+    $("#lemonperfecttxt").hover(function(){
+        if (bc==true) {
+          
+            $( "#lemonperfectimg1" ).css("display", "inherit");
+            $( "#lemonperfectimg2" ).css("display", "inherit");
+            
+            bc=false;
+        }
+        else {
+
+            $( "#lemonperfectimg1" ).css("display", "none");
+            $( "#lemonperfectimg2" ).css("display", "none");
+
+            bc=true;
+            
+        }   
+    });
+
+   var cd= true;
+    $("#milkbartxt").hover(function(){
+        if (cd==true) {
+          
+            $( "#milkbarimg1" ).css("display", "inherit");
+            $( "#milkbarimg2" ).css("display", "inherit");
+            
+            cd=false;
+        }
+        else {
+
+            $( "#milkbarimg1" ).css("display", "none");
+            $( "#milkbarimg2" ).css("display", "none");
+
+            cd=true;
+            
+        }   
+    }); 
+
+   var de= true;
+    $("#smiletxt").hover(function(){
+        if (de==true) {
+          
+            $( "#smileimg1" ).css("display", "inherit");
+            $( "#smileimg2" ).css("display", "inherit");
+            
+            de=false;
+        }
+        else {
+
+            $( "#smileimg1" ).css("display", "none");
+            $( "#smileimg2" ).css("display", "none");
+
+            de=true;
+            
+        }   
+    }); 
+
+   var ef= true;
+    $("#highcourttxt").hover(function(){
+        if (ef==true) {
+          
+            $( "#highcourtimg1" ).css("display", "inherit");
+            $( "#highcourtimg2" ).css("display", "inherit");
+            
+            ef=false;
+        }
+        else {
+
+            $( "#highcourtimg1" ).css("display", "none");
+            $( "#highcourtimg2" ).css("display", "none");
+
+            ef=true;
+            
+        }   
+    });
+
+    var fg= true;
+    $("#humancotxt").hover(function(){
+        if (fg==true) {
+          
+            $( "#humancoimg1" ).css("display", "inherit");
+            $( "#humancoimg2" ).css("display", "inherit");
+            $( "#humancoimg3" ).css("display", "inherit");
+            
+            fg=false;
+        }
+        else {
+
+            $( "#humancoimg1" ).css("display", "none");
+            $( "#humancoimg2" ).css("display", "none");
+            $( "#humancoimg3" ).css("display", "none");
+
+            fg=true;
+            
+        }   
+    });
+
+
+     var gh= true;
+    $("#arftxt").hover(function(){
+        if (gh==true) {
+          
+            $( "#arfimg1" ).css("display", "inherit");
+            $( "#arfimg2" ).css("display", "inherit");
+            
+            gh=false;
+        }
+        else {
+
+            $( "#arfimg1" ).css("display", "none");
+            $( "#arfimg2" ).css("display", "none");
+
+            gh=true;
+            
+        }   
+    });       
+
+
+   var l= true;
+    $("#nav-icon3").click(function(){
+        if (l==true) {
+          
+            $( ".mobilemenu" ).css("display", "inherit");
+            
+            l=false;
+        }
+        else {
+
+            $( ".mobilemenu" ).css("display", "none");
+
+            l=true;
+            
+        }   
+    });
+
+var coll = document.getElementsByClassName("collapsible");
+var p;
+
+for (p = 0; p < coll.length; p++) {
+  coll[p].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } 
+
+    else {
+      content.style.display = "block";
+    }
+  });
+}
+
+   var q= true;
+    $("#madisontxtmobile").click(function(){
+        if (q==true) {
+          
+            $( "#madisontxtmobile" ).css("font-style", "italic");
+            $( "#madisontxtmobile" ).css("text-decoration", "line-through");
+            
+            q=false;
+        }
+        else {
+
+            $( "#madisontxtmobile" ).css("font-style", "normal");
+            $( "#madisontxtmobile" ).css("text-decoration", "none");
+
+            q=true;
+            
+        }   
+    });
+
+
+   var qq= true;
+    $("#allisontxtmobile").click(function(){
+        if (qq==true) {
+          
+            $( "#allisontxtmobile" ).css("font-style", "italic");
+            $( "#allisontxtmobile" ).css("text-decoration", "line-through");
+            
+            qq=false;
+        }
+        else {
+
+            $( "#allisontxtmobile" ).css("font-style", "normal");
+            $( "#allisontxtmobile" ).css("text-decoration", "none");
+
+            qq=true;
+            
+        }   
+    });
+
+   var r= true;
+    $("#zoetxtmobile").click(function(){
+        if (r==true) {
+          
+            $( "#zoetxtmobile" ).css("font-style", "italic");
+            $( "#zoetxtmobile" ).css("text-decoration", "line-through");
+            
+            r=false;
+        }
+        else {
+
+            $( "#zoetxtmobile" ).css("font-style", "normal");
+            $( "#zoetxtmobile" ).css("text-decoration", "none");
+
+            r=true;
+            
+        }   
+    });
+
+    var rr= true;
+    $("#toritxtmobile").click(function(){
+        if (rr==true) {
+          
+            $( "#toritxtmobile" ).css("font-style", "italic");
+            $( "#toritxtmobile" ).css("text-decoration", "line-through");
+            
+            rr=false;
+        }
+        else {
+
+            $( "#toritxtmobile" ).css("font-style", "normal");
+            $( "#toritxtmobile" ).css("text-decoration", "none");
+
+            rr=true;
+            
+        }   
+    });
+
+    var s= true;
+    $("#gracetxtmobile").click(function(){
+        if (s==true) {
+          
+            $( "#gracetxtmobile" ).css("font-style", "italic");
+            $( "#gracetxtmobile" ).css("text-decoration", "line-through");
+            
+            s=false;
+        }
+        else {
+
+            $( "#gracetxtmobile" ).css("font-style", "normal");
+            $( "#gracetxtmobile" ).css("text-decoration", "none");
+
+            s=true;
+            
+        }   
+    });
+
+    var ss= true;
+    $("#gilliantxtmobile").click(function(){
+        if (ss==true) {
+          
+            $( "#gilliantxtmobile" ).css("font-style", "italic");
+            $( "#gilliantxtmobile" ).css("text-decoration", "line-through");
+            
+            ss=false;
+        }
+        else {
+
+            $( "#gilliantxtmobile" ).css("font-style", "normal");
+            $( "#gilliantxtmobile" ).css("text-decoration", "none");
+
+            ss=true;
+            
+        }   
+    });
+
+
+    var t= true;
+    $("#hawatxtmobile").click(function(){
+        if (t==true) {
+          
+            $( "#hawatxtmobile" ).css("font-style", "italic");
+            $( "#hawatxtmobile" ).css("text-decoration", "line-through");
+            
+            t=false;
+        }
+        else {
+
+            $( "#hawatxtmobile" ).css("font-style", "normal");
+            $( "#hawatxtmobile" ).css("text-decoration", "none");
+
+            t=true;
+            
+        }   
+    });
+
+    var u= true;
+    $("#nikoletxtmobile").click(function(){
+        if (u==true) {
+          
+            $( "#nikoletxtmobile" ).css("font-style", "italic");
+            $( "#nikoletxtmobile" ).css("text-decoration", "line-through");
+            
+            u=false;
+        }
+        else {
+
+            $( "#nikoletxtmobile" ).css("font-style", "normal");
+            $( "#nikoletxtmobile" ).css("text-decoration", "none");
+
+            u=true;
+            
+        }   
+    });
+
+    var aa= true;
+    $("#judytxtmobile").click(function(){
+        if (aa==true) {
+          
+            $( "#judytxtmobile" ).css("font-style", "italic");
+            $( "#judytxtmobile" ).css("text-decoration", "line-through");
+            
+            aa=false;
+        }
+        else {
+
+            $( "#judytxtmobile" ).css("font-style", "normal");
+            $( "#judytxtmobile" ).css("text-decoration", "none");
+
+            aa=true;
+            
+        }   
+    });
+
+    var bb= true;
+    $("#aboutfacetxtmobile").click(function(){
+        if (bb==true) {
+          
+            $( "#aboutfacetxtmobile" ).css("font-style", "italic");
+            $( "#aboutfacetxtmobile" ).css("text-decoration", "line-through");
+            
+            bb=false;
+        }
+        else {
+
+            $( "#aboutfacetxtmobile" ).css("font-style", "normal");
+            $( "#aboutfacetxtmobile" ).css("text-decoration", "none");
+
+            bb=true;
+            
+        }   
+    });
+
+    var cc= true;
+    $("#snowdaystxtmobile").click(function(){
+        if (cc==true) {
+          
+            $( "#snowdaystxtmobile" ).css("font-style", "italic");
+            $( "#snowdaystxtmobile" ).css("text-decoration", "line-through");
+            
+            cc=false;
+        }
+        else {
+
+            $( "#snowdaystxtmobile" ).css("font-style", "normal");
+            $( "#snowdaystxtmobile" ).css("text-decoration", "none");
+
+            cc=true;
+            
+        }   
+    });
+
+    var dd= true;
+    $("#humancotxtmobile").click(function(){
+        if (dd==true) {
+          
+            $( "#humancotxtmobile" ).css("font-style", "italic");
+            $( "#humancotxtmobile" ).css("text-decoration", "line-through");
+            
+            dd=false;
+        }
+        else {
+
+            $( "#humancotxtmobile" ).css("font-style", "normal");
+            $( "#humancotxtmobile" ).css("text-decoration", "none");
+
+            dd=true;
+            
+        }   
+    });
+
+    var ee= true;
+    $("#ladidatxtmobile").click(function(){
+        if (ee==true) {
+          
+            $( "#ladidatxtmobile" ).css("font-style", "italic");
+            $( "#ladidatxtmobile" ).css("text-decoration", "line-through");
+            
+            ee=false;
+        }
+        else {
+
+            $( "#ladidatxtmobile" ).css("font-style", "normal");
+            $( "#ladidatxtmobile" ).css("text-decoration", "none");
+
+            ee=true;
+            
+        }   
+    });
+
+    var ff= true;
+    $("#highcourttxtmobile").click(function(){
+        if (ff==true) {
+          
+            $( "#highcourttxtmobile" ).css("font-style", "italic");
+            $( "#highcourttxtmobile" ).css("text-decoration", "line-through");
+            
+            ff=false;
+        }
+        else {
+
+            $( "#highcourttxtmobile" ).css("font-style", "normal");
+            $( "#highcourttxtmobile" ).css("text-decoration", "none");
+
+            ff=true;
+            
+        }   
+    });
+
+    var gg= true;
+    $("#smiletxtmobile").click(function(){
+        if (gg==true) {
+          
+            $( "#smiletxtmobile" ).css("font-style", "italic");
+            $( "#smiletxtmobile" ).css("text-decoration", "line-through");
+            
+            gg=false;
+        }
+        else {
+
+            $( "#smiletxtmobile" ).css("font-style", "normal");
+            $( "#smiletxtmobile" ).css("text-decoration", "none");
+
+            gg=true;
+            
+        }   
+    });
+
+
+    var hh= true;
+    $("#muthatxtmobile").click(function(){
+        if (hh==true) {
+          
+            $( "#muthatxtmobile" ).css("font-style", "italic");
+            $( "#muthatxtmobile" ).css("text-decoration", "line-through");
+            
+            hh=false;
+        }
+        else {
+
+            $( "#muthatxtmobile" ).css("font-style", "normal");
+            $( "#muthatxtmobile" ).css("text-decoration", "none");
+
+            hh=true;
+            
+        }   
+    });
+
+    var hi= true;
+    $("#lemonperfecttxtmobile").click(function(){
+        if (hi==true) {
+          
+            $( "#lemonperfecttxtmobile" ).css("font-style", "italic");
+            $( "#lemonperfecttxtmobile" ).css("text-decoration", "line-through");
+            
+            hi=false;
+        }
+        else {
+
+            $( "#lemonperfecttxtmobile" ).css("font-style", "normal");
+            $( "#lemonperfecttxtmobile" ).css("text-decoration", "none");
+
+            hi=true;
+            
+        }   
+    });
+
+    var ii= true;
+    $("#milkbartxtmobile").click(function(){
+        if (ii==true) {
+          
+            $( "#milkbartxtmobile" ).css("font-style", "italic");
+            $( "#milkbartxtmobile" ).css("text-decoration", "line-through");
+            
+            ii=false;
+        }
+        else {
+
+            $( "#milkbartxtmobile" ).css("font-style", "normal");
+            $( "#milkbartxtmobile" ).css("text-decoration", "none");
+
+            ii=true;
+            
+        }   
+    });
+
+
+    var jj= true;
+    $("#arftxtmobile").click(function(){
+        if (jj==true) {
+          
+            $( "#arftxtmobile" ).css("font-style", "italic");
+            $( "#arftxtmobile" ).css("text-decoration", "line-through");
+            
+            jj=false;
+        }
+        else {
+
+            $( "#arftxtmobile" ).css("font-style", "normal");
+            $( "#arftxtmobile" ).css("text-decoration", "none");
+
+            jj=true;
+            
+        }   
+    });
+	
+}); 
